3f7bff462d2681daed42031853569b8777e3195b
luci <gfuonxner@gmail.com>
2025-07-13 18:17:24 -0500

    added 2 new functions

--- encoder-adv.py
+++ encoder-adv.py
@@ -2,4 +2,37 @@ import string
 import random
 import json
 
+def generate_cipher_key():
+    alphabet = string.ascii_lowercase
+    shuffled_alphabet = list(alphabet)
+    random.shuffle(shuffled_alphabet)
+
+
+    cipher_key = dict(zip(alphabet, shuffled_alphabet))
+    return cipher_key
+
+def encrypt_message(message_text, cipher_key):
+    message_text = message_text.lower()
+    encrypted_text = ""
+
+    for original_character in message_text:
+        if original_character in cipher_key:
+            encrypted_text += cipher_key[original_character]
+        else:
+            encrypted_text += original_character  # keep punctuation/spaces
+
+    return encrypted_text
+
+def encrypt_message(message_text, cipher_key):
+    message_text = message_text.lower()
+    encrypted_text = ""
+
+    for original_character in message_text:
+        if original_character in cipher_key:
+            encrypted_text += cipher_key[original_character]
+        else:
+            encrypted_text += original_character  # keep punctuation/spaces
+
+    return encrypted_text
+
 
